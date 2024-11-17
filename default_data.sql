bc12b215117155116669e13a573663fd8e9a1448
Sormunen Tiina M <tiinasor@cubbli22-gold.local>
2024-11-17 22:57:16 +0200

    Add default data for locations and categories

---
+++ default_data.sql
@@ -0,0 +1,30 @@
+INSERT INTO locations (name) VALUES
+    ('Uusimaa'),
+    ('Varsinais-Suomi'),
+    ('Satakunta'),
+    ('Kanta-Häme'),
+    ('Pirkanmaa'),
+    ('Päijät-Häme'),
+    ('Kymenlaakso'),
+    ('Etelä-Karjala'),
+    ('Etelä-Savo'),
+    ('Pohjois-Savo'),
+    ('Pohjois-Karjala'),
+    ('Keski-Suomi'),
+    ('Etelä-Pohjanmaa'),
+    ('Pohjanmaa'),
+    ('Keski-Pohjanmaa'),
+    ('Pohjois-Pohjanmaa'),
+    ('Kainuu'),
+    ('Lappi'),
+    ('Ahvenanmaa');
+
+INSERT INTO categories (name)
+VALUES 
+    ('Jyrsijät'),
+    ('Kissat'),
+    ('Koirat'),
+    ('Linnut'),
+    ('Matelijat'),
+    ('Muut');
+    
